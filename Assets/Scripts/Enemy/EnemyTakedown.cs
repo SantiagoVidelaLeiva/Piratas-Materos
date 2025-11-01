fb82d7887c680804fa2117f63402724bc6d4f4b7
LucasCuello <lucascuello8@gmail.com>
2025-11-01 19:21:02 -0300

    Fix Enemigos invencibles
    + Ahora en EnemyTakedown hay una referencia al EnemyControllerBase del enemigo y solo se habilita la acción si el enemigo esta en modo Suspicious o Patrolling. En Danger se desactiva.

--- Assets/Scripts/Enemy/EnemyTakedown.cs
+++ Assets/Scripts/Enemy/EnemyTakedown.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class EnemyTakedown : MonoBehaviour, IInteractable
 {
     private CharacterHealth _enemyHealth;
+    private EnemyControllerBase _enemyController;
+    private BoxCollider _boxColl;
     private bool _hasBeenUsed = false;
 
     [SerializeField] private string _interactPrompt = "E to takedown";
@@ -11,9 +13,30 @@ public class EnemyTakedown : MonoBehaviour, IInteractable
     private void Awake()
     {
         _enemyHealth = GetComponentInParent<CharacterHealth>();
+        _enemyController = GetComponentInParent<EnemyControllerBase>();
+
+        _boxColl = GetComponent<BoxCollider>();
+
         if (_enemyHealth == null)
         {
-            UnityEngine.Debug.LogError("No se encontró el componente CharacterHealth en este objeto o en su padre.");
+            UnityEngine.Debug.LogError("No se encontrï¿½ el componente CharacterHealth en este objeto o en su padre.");
+        }
+        if (_enemyController == null)
+        {
+            UnityEngine.Debug.LogError("No se encontrï¿½ el componente EnemyController en este objeto o en su padre.");
+        }
+    }
+
+    void Update()
+    {
+        if (_enemyController.State == EnemyControllerBase.EnemyState.Danger)
+        {
+            _boxColl.enabled = false;
+        }
+
+        if (_enemyController.State == EnemyControllerBase.EnemyState.Suspicious || _enemyController.State == EnemyControllerBase.EnemyState.Patrolling)
+        {
+            _boxColl.enabled = true;
         }
     }
 
@@ -31,12 +54,13 @@ public class EnemyTakedown : MonoBehaviour, IInteractable
 
         if (_enemyHealth == null)
         {
-            UnityEngine.Debug.LogError("La referencia a CharacterHealth no está asignada en el Inspector. Por favor, arrastra el objeto padre del enemigo al campo '_enemyHealth' del script EnemyTakedown.");
+            UnityEngine.Debug.LogError("La referencia a CharacterHealth no estï¿½ asignada en el Inspector. Por favor, arrastra el objeto padre del enemigo al campo '_enemyHealth' del script EnemyTakedown.");
             return false;
         }
 
-        
+
         _enemyHealth.TakeDamage(1000f);
+        gameObject.SetActive(false);
 
         return true;
     }
