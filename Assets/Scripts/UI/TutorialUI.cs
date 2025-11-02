ef1a36d8db9c41b161f3e6ec42fb8459ebafe3ea
SantiagoVidelaLeiva <82802066+SantiagoVidelaLeiva@users.noreply.github.com>
2025-11-02 11:36:23 -0300

    Update TutorialUI.cs

--- Assets/Scripts/UI/TutorialUI.cs
+++ Assets/Scripts/UI/TutorialUI.cs
@@ -21,7 +21,6 @@ public class TutorialUI : MonoBehaviour
                 { 1, "ASWD son el movimiento del personaje pero tambien puedes usar las flechas" },
                 { 2, "CTRL para agacharse, Shift para correr y usa el mouse para mover la camara" },
                 { 3, "Presiona C  para cambiar de modo y en el otro modo usa las flechas para cambiar de camara" },
-                { 4, "Prueba usar la tecla E detras de este enemigo, �Buena Suerte y Saludos! :)" }
             };
 
             ShowStep(0);
